a612758611cdfc68820ec9be7f56bb01a89e0779
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2022-12-14 22:23:47 -0700

    Update pyenv_ddev_patch.sh

--- pyenv_ddev_patch.sh
+++ pyenv_ddev_patch.sh
@@ -1,7 +1,7 @@
   #!/usr/bin/env bash
 
   #install pip update
-  echo -e "Installing pip update"
+  echo -e "\nInstalling pip update"
 
   /home/vagrant/.pyenv/versions/3.8-dev/bin/python3.8 -m pip install --upgrade pip
   python -m pip install --user pipx
@@ -9,16 +9,16 @@
   python3 -m pip install --user -U pipx
 
   #installing memray modules
-  echo -e "Installing memray modules"
+  echo -e "\nInstalling memray modules"
 
-  sudo apt-get install libunwind-dev
-  sudo apt-get install liblz4-dev
+  sudo apt-get -y install libunwind-dev
+  sudo apt-get -y install liblz4-dev
   python3 -m pip install memray
 
   #install ddev
-  echo -e "Attempting complete solve of ddev"
+  echo -e "\nAttempting complete solve of ddev"
   pip install ddev
 
   #Ddev install confirmation
-  echo  -e "nConfirming Ddev  install"
+  echo  -e "\nConfirming Ddev  install"
   ddev
