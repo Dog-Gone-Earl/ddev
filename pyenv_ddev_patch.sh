3611bd3691339bb1fbe1256310f2f67d2e2f8224
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2022-12-14 22:15:24 -0700

    Update pyenv_ddev_patch.sh

--- pyenv_ddev_patch.sh
+++ pyenv_ddev_patch.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/env bash
+  #!/usr/bin/env bash
 
-#install pip update
-echo -e "Installing pip update"
+  #install pip update
+  echo -e "Installing pip update"
 
-/home/vagrant/.pyenv/versions/3.8-dev/bin/python3.8 -m pip install --upgrade pip
-python -m pip install --user pipx
-python3 -m pipx ensurepath
-python3 -m pip install --user -U pipx
+  /home/vagrant/.pyenv/versions/3.8-dev/bin/python3.8 -m pip install --upgrade pip
+  python -m pip install --user pipx
+  python3 -m pipx ensurepath
+  python3 -m pip install --user -U pipx
 
-#installing memray modules
-echo -e "Installing memray modules"
+  #installing memray modules
+  echo -e "Installing memray modules"
 
-sudo apt-get install libunwind-dev
-sudo apt-get install liblz4-dev
-python3 -m pip install memray
+  sudo apt-get install libunwind-dev
+  sudo apt-get install liblz4-dev
+  python3 -m pip install memray
 
-#install ddev
-echo -e "Attempting complete solve of ddev"
-pip install ddev
+  #install ddev
+  echo -e "Attempting complete solve of ddev"
+  pip install ddev
 
-#Ddev install confirmation
-echo  -e "nConfirming Ddev  install"
-ddev
+  #Ddev install confirmation
+  echo  -e "nConfirming Ddev  install"
+  ddev
