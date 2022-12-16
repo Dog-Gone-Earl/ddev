033e2972250f404d495c3332f874b9b754877117
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2022-12-15 19:08:57 -0700

    Update pyenv_ddev_patch.sh

--- pyenv_ddev_patch.sh
+++ pyenv_ddev_patch.sh
@@ -1,7 +1,7 @@
   #!/usr/bin/env bash
 
   #install pip update
-  echo -e "\nInstalling pip update"
+  echo "\nInstalling pip update"
 
   /home/vagrant/.pyenv/versions/3.8-dev/bin/python3.8 -m pip install --upgrade pip
   python -m pip install --user pipx
@@ -9,21 +9,21 @@
   python3 -m pip install --user -U pipx
 
   #installing memray modules
-  echo -e "\nInstalling memray modules"
+  echo "\nInstalling memray modules"
 
   sudo apt-get -y install libunwind-dev
   sudo apt-get -y install liblz4-dev
   python3 -m pip install memray
 
   #install ddev
-  echo -e "\nAttempting complete solve of ddev"
+  echo "\nAttempting complete solve of ddev"
   pip install "datadog-checks-dev[cli]"
   
-  echo -e "Install of Docker"
+  echo "Install of Docker"
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh ./get-docker.sh
   sudo usermod -aG docker $USER
   
   #Ddev install confirmation
-  echo  -e "\nConfirming Ddev  install"
+  echo "\nConfirming Ddev  install"
   ddev
